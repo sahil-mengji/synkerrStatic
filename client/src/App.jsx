35fdceff8c65aadd5953d239b8936460b8474442
alphaplayerofdooms <nikhilkottoli2005@gmail.com>
2024-04-21 19:27:27 +0530

    progress on static website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className='h-screen w-full bg-slate-300 text-5xl'>
-      Synker
+    <div className="App bg-black min-h-screen">
+      <header className="App-header">
+        <div className='LogoAndText'>
+          <img src="/image1.png" alt="logo" />
+          <h1>Synkerr.</h1>
+        </div>
+        <div className='three-on-top'>
+        <h3>Our Aim</h3>
+        <h3>Team</h3>
+        <h3 className='lastOne'>What we do</h3>
+        </div>
+        <p className="contact-button">Contact Us</p>
+      </header>
+
+      <body className='Body'>
+        <div className='bodyI'>
+          <img src="/image4.png" className="bodyI1" alt="image4" />
+          <p>Exploit the power of</p>
+          <img src="/image5.png" className="bodyI2" alt="image5" />
+        </div>
+        <p>Purposful Connections</p>
+
+        <div className='WaitList'>
+          <img src="/image6.png" className="WaitList" alt="image6" />
+          <p>Join the Waitlist -{'>'} </p>
+        </div>
+        <img src="/image7.png" className='beta' alt="image7" />
+        <div className='scroll'>
+          <img src="/image8.png" className='prop1' alt="image8" />
+          <p className='ST'>Scroll Down</p>
+          <img src="/image9.png" className='prop2' alt="image9" />
+        </div>
+        
+      </body>
     </div>
-  )
+
+  );
 }
 
-export default App
+export default App;
