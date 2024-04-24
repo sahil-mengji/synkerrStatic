c9305d607a190b8c178808de69165c3a707ec21d
alphaplayerofdooms <nikhilkottoli2005@gmail.com>
2024-04-24 18:32:48 +0530

    named it better and organised into div

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,31 @@ function App() {
   return (
     <div className="App bg-black min-h-screen">
       <header className="App-header">
-        <div className='LogoAndText'>
-          <img src="/image1.png" alt="logo" />
-          <h1>Synkerr.</h1>
-        </div>
-        <div className='three-on-top'>
-        <h3>Our Aim</h3>
-        <h3>Team</h3>
-        <h3 className='lastOne'>What we do</h3>
-        </div>
-        <p className="contact-button">Contact Us</p>
+        //idk what to put here
       </header>
 
       <body className='Body'>
+      <div className='HeroSection'>
+        <div className='NavBar'>
+          <div className='LogoAndText'>
+            <img src="/image1.png" alt="logo" />
+            <h1>Synkerr.</h1>
+          </div>
+          <div className='three-on-top'>
+            <h3>Our Aim</h3>
+            <h3>Team</h3>
+            <h3 className='lastOne'>What we do</h3>
+          </div>
+          <p className="contact-button">Contact Us</p>
+        </div>
         <div className='HeroText'>
           <p className='HeroText1'>Exploit the power of</p>
           <p className='HeroText2'>Purposful Connections</p>
           <p className='HeroText3'>Join the Waitlist -{'>'} </p>
           <img src="/image7.png" className='beta' alt="image7" />
         </div>
+      </div>
+        
       </body>
     </div>
 
