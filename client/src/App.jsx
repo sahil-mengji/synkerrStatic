04aada74c7a04821511356486da0044f7c647ac4
alphaplayerofdooms <nikhilkottoli2005@gmail.com>
2024-04-27 23:06:18 +0530

    tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
+
 import React from 'react'
 import HeroSection from './components/HeroSection'
 import Globe from './components/Globe'
 import Carousel from './components/Carousel'
+import { useRef } from 'react'
 const App = () => {
+  const bottomRef = useRef(null);
   return (
     <div className='App'>
-      <HeroSection/>
-      <Globe/>
+      <HeroSection bottomRef={bottomRef}/>
+      <div className='Globe' ref={bottomRef}><Globe/></div>
+      
       <Carousel/>
     </div>
-    
-
   )
 }
 export default App
--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import './HeroSection.css';
+import React, { useRef } from 'react';
 
-function HeroSection() {
-    return (
-      <div className="App bg-black min-h-screen">
-        <header className="App-header">
-        </header>
-  
-        <body className='Body'>
-        <div className='HeroSection'>
-          <div className='NavBar'>
-            <div className='LogoAndText'>
-              <img src="/image1.png" alt="logo" />
-              <h1>Synkerr.</h1>
+
+function HeroSection({bottomRef}) {
+  const scrollToBottom = () => {
+    bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+  return (
+    <div className="App bg-black min-h-screen">
+      <body className='Body'>
+        <div className='position relative'>
+          <div className='flex justify-between items-center'>
+            <img src="logo.svg" alt="logo" />
+            <div className='flex justify-between w-1/4  text-lg rounded-full'>
+              <h3 className='text-gray-400'>Our Aim</h3>
+              <h3 className='text-gray-400'>Team</h3>
+              <h3 className='text-gray-400'>What we do</h3>
             </div>
-            <div className='three-on-top'>
-              <h3>Our Aim</h3>
-              <h3>Team</h3>
-              <h3 className='lastOne'>What we do</h3>
-            </div>
-            <p className="contact-button">Contact Us</p>
+            <p className="bg-[#302D50] text-white text-base px-5 py-2 rounded-full">Contact Us</p>
           </div>
-          <div className='HeroText'>
-            <p className='HeroText1'>Exploit the power of</p>
-            <p className='HeroText2'>Purposful Connections</p>
-            <p className='HeroText3'>Join the Waitlist -{'>'} </p>
-            <img src="/image7.png" className='beta' alt="image7" />
-            <div className='ScrollDownButton'></div>
-            <p className='ScrollDownText'>Scroll Down  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100" rel="stylesheet" />
-            <span class="material-symbols-outlined">arrow_downward</span></p>
-          </div>
+          <div className="flex flex-col items-center mt-10 text-white bg-no-repeat" style={{backgroundImage: "url('./image4.png'), url('./image5.png')",
+            backgroundSize: '30%, contain',backgroundPosition: '10%, 85%'}}>
+            <p className='text-[#A9A9A9] text-4xl mb-6'>Exploit the power of</p>
+            <p className='text-white mb-6 text-5xl'>Purposful Connections</p>
+            <p className='text-black text-3xl font-bold bg-[#A79FFF] py-5 px-8 rounded-[40px] mb-6'>Join the Waitlist -{'>'} </p>
+            <div>🎉 <span className="text-yellow-500"> BETA</span> version Dropping Soon</div>
+            <div className="flex text-base bg-black border border-[#888888] px-5 py-2 rounded-[27px] cursor-pointer mt-20 text-[#888888]" style={{ color: '#888888'}} onClick={scrollToBottom}>
+          Scroll Down <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100" rel="stylesheet" />
+          <span className="material-symbols-outlined">arrow_downward</span>
         </div>
-        <div className='ConnectTeamUpCollaborate'>
-          <img src="/image9.png" />
         </div>
-        </body>
+        </div>
+        <img src="/image9.png" className="absolute h-[25%] w-[15%]" />
+      </body>
     </div>
-    );
+  );
 }
 
 export default HeroSection;
