2b0c3b946aa1087b1b44abd4c2d274d0e766d39e
alphaplayerofdooms <nikhilkottoli2005@gmail.com>
2024-04-29 13:22:52 +0530

    tailwind

--- client/src/components/Globe.jsx
+++ client/src/components/Globe.jsx
@@ -3,12 +3,11 @@ import GlobeA from './GlobeAnimation';
 
 function Globe() {
   return (
-    <div className='flex justify-center items-center relative'>
-      <div className='absolute z-0 flex justify-center items-center m-96'>
-        <GlobeA className='relative' />
-      </div>
-      <div className='z-1 relative text-white mb-20 -ml-10 flex flex-col items-center text-3xl bg-[rgba(91,86,145,0.3)] w-auto p-2 rounded-lg shadow-md border border-[rgba(255,255,255,0.37)]'>
-        <p className='text-white'>Random Ass Shit</p>
+    <div className="flex justify-center items-center relative mb-80">
+      <div className="absolute z-0 flex justify-center items-center m-80">
+        <GlobeA className="relative"/>
+        <p className="z-1 absolute top-20 right-0 text-white flex flex-col bg-contain items-start text-3xl bg-[rgba(91,86,145,0.3)] p-2 rounded-lg shadow-md border border-[rgba(255,255,255,0.37)] text-white">Random Ass Shit</p>
+        <p className="z-1 absolute bottom-20 left-0 text-white flex flex-col bg-contain items-start text-3xl bg-[rgba(91,86,145,0.3)] p-2 rounded-lg shadow-md border border-[rgba(255,255,255,0.37)] text-white">Random Ass Shit</p>
       </div>
     </div>
   );
