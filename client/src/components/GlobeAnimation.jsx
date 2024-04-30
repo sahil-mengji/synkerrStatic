764e0a88b6b561dbd6a33309003d6a00d48f3e99
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2024-04-30 11:46:45 +0530

    the globe section should be responsive

--- client/src/components/GlobeAnimation.jsx
+++ client/src/components/GlobeAnimation.jsx
@@ -1,11 +1,9 @@
-import "./GlobeAnimation.css";
-
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
 let w,h;
-if (window.matchMedia("(min-width: 640px)").matches && window.matchMedia("(max-width: 1240px)").matches) {
-  w = 1.5;
-  h = 1.5;
+if (window.matchMedia("(max-width: 640px)").matches){
+  w = 1.3;
+  h = 1.3;
 }
 else{
   w=2,h=2;
@@ -31,13 +29,10 @@ export default function App() {
       markerColor: [0.1, 0.8, 1],
       glowColor: [1, 1, 1],
       markers: [
-        // longitude latitude
         { location: [37.7595, -122.4367], size: 0.03 },
         { location: [40.7128, -74.006], size: 0.1 }
       ],
       onRender: (state) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
         state.phi = phi;
         phi += 0.01;
       }
@@ -53,7 +48,7 @@ export default function App() {
     <div className="GlobeA">
       <canvas
         ref={canvasRef}
-        style={{ width: 600, height: 600, maxWidth: "1000%", aspectRatio: 1 }}
+        style={{ width: 600, height: 600, maxWidth: "10000%", aspectRatio: 1 }}
       />
     </div>
   );
