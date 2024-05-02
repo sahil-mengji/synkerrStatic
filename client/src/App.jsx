a72158a6f709a4889603fb55ee3a4395b38e40c8
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2024-05-02 17:39:05 +0530

    Three D buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,17 @@ import Globe from './components/Globe';
 import Carousel from './components/Carousel';
 import SocialMedia from './components/SocialMedia';
 
+
 const App = () => {
   const bottomRef = useRef(null);
   const TeamRef = useRef(null);
 
   return (
     <div className="App">
-      <HeroSection bottomRef={bottomRef} TeamRef={TeamRef} />
-      <div className="Globe" ref={bottomRef}> <Globe/>  </div>
-      <div className="SocialMedia"> <SocialMedia/> </div>
-      <div className="Carousel" ref={TeamRef}>  <Carousel/>  </div>
+      <HeroSection bottomRef={bottomRef} />
+      <Globe />
+      <Carousel TeamRef={TeamRef} />
+      <SocialMedia TeamRef={TeamRef} />
     </div>
   );
 };
--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './SocialMedia.css';
+import { BsLinkedin, BsInstagram, BsTwitter } from 'react-icons/bs';
 
 function SocialMedia() {
   return (
     <div className="bg-[#A79FFF] flex flex-col md:flex-row p-10">
       <div className="w-full md:w-[50%] p-4">
         <h1 className="text-black text-4xl md:text-8xl font-bold font-gilroy">
-          Who <br /> Are We ?
+          Who <br />
+          Are We ?
         </h1>
         <br />
         <p className="text-black text-lg md:text-2xl">
@@ -21,26 +24,22 @@ function SocialMedia() {
           <p className="text-[#2C1A4D] text-2xl md:text-3xl mb-4 md:mb-0">
             Check Out our <br /> social media Hangle
           </p>
-          <div className='flex justify-center'><img className="w-32 h-32 md:w-64 md:h-64" src="SocialMediaArrow.svg" alt="Social Media Arrow" /></div>
-          
+          <div className="flex justify-center">
+            <img className="w-32 h-32 md:w-64 md:h-64" src="SocialMediaArrow.svg" alt="Social Media Arrow" />
+          </div>
         </div>
-        <div>
-          <div className="flex justify-center mt-4">
-            <a href="https://www.linkedin.com/company/synkerr/" target="_blank" rel="noopener noreferrer" className="mr-4">
-              <img src="LinkedIn.png" alt="LinkedIn" />
+        <div className="flex flex-col md:flex-col justify-center">
+          <div className="flex TD justify-center ">
+            <a href="https://www.linkedin.com/company/synkerr/" target="_blank" rel="noopener noreferrer" className="mr-4 flex flex-col">
+              <BsLinkedin size={48} /><span>LinkedIn</span>
             </a>
-            <a href="https://www.instagram.com/synkerrofficial?igsh=MXExNzNhaDIxbGF5dA==" rel="noopener noreferrer" target="_blank" className="mr-4">
-              <img src="Instagram.png" alt="Instagram" />
+            <a href="https://www.instagram.com/synkerrofficial?igsh=MXExNzNhaDIxbGF5dA==" rel="noopener noreferrer" target="_blank" className="mr-4 flex flex-col">
+              <BsInstagram size={48} /><span>Instagram</span>
             </a>
-          </div>
-          <div>
-            <a href="https://twitter.com/Kakashi8654" target="_blank" rel="noopener noreferrer">
-                <div className="flex justify-center mt-4 h-24 sm:h-[100%]">
-                <img src="Twitter.png" alt="Twitter" />
-                </div>
+            <a href="https://twitter.com/Kakashi8654" target="_blank" rel="noopener noreferrer" className='flex flex-col mr-4'>
+              <BsTwitter size={48} /><span>Twitter</span>
             </a>
           </div>
-          
         </div>
       </div>
     </div>
